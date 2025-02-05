89be1d5d48c61fad6ae6aae4b5266ec37f7c8612
winstencoellins <winstencoellins13@gmail.com>
2025-02-06 00:39:30 +0700

    add: work order form

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,4 +1,11 @@
+'use client'
+
+import { useState } from "react"
+
 export default function CreateInvoices() {
+    const [isLoding, setIsLoading] = useState<boolean>(false)
+    const [error, setError] = useState<string | null>(null)
+
     return (
         <>
         </>
--- app/dashboard/work-orders/create/page.tsx
+++ app/dashboard/work-orders/create/page.tsx
@@ -2,8 +2,41 @@
 
 import { Form } from "@heroui/form"
 import { Input } from "@heroui/input"
+import { FormEvent, useState } from "react"
 
 export default function CreateWorkOrder() {
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [message, setMessage] = useState<string>('')
+    const [error, setError] = useState<string | null>(null)
+
+    const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        setIsLoading(true)
+
+        try {
+            const formData = new FormData(event.currentTarget)
+            const response = await fetch('/api/work-orders', {
+                method: "POST",
+                body: formData
+            })
+
+            if (!response.ok) {
+                throw new Error('Failed to submit the data. Please try again.')
+            }
+
+            const data = await response.json()
+
+            if (data) {
+                setMessage('Work Order created successfully')
+            }
+        } catch (error: any) {
+            setError(error.message)
+            console.log(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
     return (
         <>
             <header className="bg-white shadow-sm">
@@ -13,13 +46,13 @@ export default function CreateWorkOrder() {
 
             </header>
             <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <Form validationBehavior="native">
+                <Form validationBehavior="native" onSubmit={onSubmit}>
                     <Input
                         isRequired
                         label="Work Order Number"
                         labelPlacement="outside"
                         name="workOrderNumber"
-                        placeholder="0001"
+                        placeholder="AZELF001"
                         type="text"
                         validate={(value) => {
                         if (value.length < 3) {
@@ -31,10 +64,10 @@ export default function CreateWorkOrder() {
                     />
                     <Input
                         isRequired
-                        label="Work Order Number"
+                        label="Furniture Size"
                         labelPlacement="outside"
-                        name="workOrderNumber"
-                        placeholder="0001"
+                        name="furnitureSize"
+                        placeholder="10x20x30"
                         type="text"
                         validate={(value) => {
                         if (value.length < 3) {
@@ -46,11 +79,10 @@ export default function CreateWorkOrder() {
                     />
                     <Input
                         isRequired
-                        label="Work Order Number"
+                        label="Estimated Finish Date"
                         labelPlacement="outside"
-                        name="workOrderNumber"
-                        placeholder="0001"
-                        type="text"
+                        name="estimatedFinishDate"
+                        type="date"
                         validate={(value) => {
                         if (value.length < 3) {
                             return "Username must be at least 3 characters long";
@@ -61,10 +93,10 @@ export default function CreateWorkOrder() {
                     />
                     <Input
                         isRequired
-                        label="Work Order Number"
+                        label="Price"
                         labelPlacement="outside"
-                        name="workOrderNumber"
-                        placeholder="0001"
+                        name="price"
+                        placeholder="10000"
                         type="text"
                         validate={(value) => {
                         if (value.length < 3) {
@@ -74,21 +106,9 @@ export default function CreateWorkOrder() {
                         return value === "admin" ? "Nice try!" : null;
                         }}
                     />
-                    <Input
-                        isRequired
-                        label="Work Order Number"
-                        labelPlacement="outside"
-                        name="workOrderNumber"
-                        placeholder="0001"
-                        type="text"
-                        validate={(value) => {
-                        if (value.length < 3) {
-                            return "Username must be at least 3 characters long";
-                        }
-
-                        return value === "admin" ? "Nice try!" : null;
-                        }}
-                    />
+                    <button type="submit" disabled={isLoading}>
+                        {isLoading ? 'Loading ...' : 'Submit'}
+                    </button>
                 </Form>
             </div>
         </>
